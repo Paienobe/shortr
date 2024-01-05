c05d892609db45a3dcdcae37803be4ad218bda67
Progress Aienobe <paienobe@gmail.com>
2024-01-05 19:53:01 +0100

    cors-update

--- utils/enable-cors.go
+++ utils/enable-cors.go
@@ -4,4 +4,5 @@ import "net/http"
 
 func EnableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 }

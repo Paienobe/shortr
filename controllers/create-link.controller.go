1fe9e4a69f0d5906d5f5429a28986d141c84705a
Progress Aienobe <paienobe@gmail.com>
2024-01-05 16:32:49 +0100

    cors-enabler

--- controllers/create-link.controller.go
+++ controllers/create-link.controller.go
@@ -14,6 +14,7 @@ import (
 )
 
 func CreateLink(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	utils.EnableCors(&w)
 	rawBody, err := io.ReadAll(r.Body)
 	if err != nil {
 		log.Fatal(err)
--- controllers/visit-short.controller.go
+++ controllers/visit-short.controller.go
@@ -6,10 +6,12 @@ import (
 	"net/http"
 
 	"github.com/Paienobe/go-url-shortener/queries"
+	"github.com/Paienobe/go-url-shortener/utils"
 	"github.com/gorilla/mux"
 )
 
 func VisitShort(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	utils.EnableCors(&w)
 	vars := mux.Vars(r)
 	shortKeyParam := vars["short_key"]
 	var originalUrl string

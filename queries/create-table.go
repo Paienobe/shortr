8587c363a2bdb2d6a55f0184ca20b1c3dae9d431
Progress Aienobe <paienobe@gmail.com>
2024-01-05 17:02:01 +0100

    query_table

--- queries/create-table.go
+++ queries/create-table.go
@@ -4,6 +4,6 @@ const CreateTableQuery = `CREATE TABLE IF NOT EXISTS link (
 		id BIGSERIAL NOT NULL PRIMARY KEY,
 		long_url VARCHAR(1500) NOT NULL ,
 		short_key VARCHAR(7) NOT NULL,
-		short_url VARCHAR(30) NOT NULL,
+		short_url VARCHAR(40) NOT NULL,
 		UNIQUE(short_key)
 	)`

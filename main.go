f766eca6b36f0baeb3870b7bd5e17146fc076e08
Progress Aienobe <paienobe@gmail.com>
2024-01-06 14:21:28 +0100

    you-guessed-it-cors-again

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 	router := mux.NewRouter()
 
 	router.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://shortr-ui.vercel.app", "http://localhost:5173"},
+		AllowedOrigins:   []string{"*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"*"},
 		ExposedHeaders:   []string{"Link"},
